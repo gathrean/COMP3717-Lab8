123ea56a45bcbfad84d1c1834c98d5f653123691
Gathrean Dela Cruz <gathrean@icloud.com>
2023-11-13 12:21:55 -0800

    Added my name and ID

---
+++ app/src/main/java/com/example/lab8gathreandelacruz/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.lab8gathreandelacruz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab8gathreandelacruz.ui.theme.Lab8GathreanDelaCruzTheme
+
+/**
+ * Gathrean Dela Cruz, A01167248
+ */
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
